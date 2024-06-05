7065c1b77beff785bf95c0798fc7d8506a7445b2
Md Altaf Hoshain <altaf@squaregroup.com>
2024-06-05 11:51:42 +0600

    Initial Commit

---
+++ SwiftUIWithMVVM/SwiftUIWithMVVMApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIWithMVVMApp.swift
+//  SwiftUIWithMVVM
+//
+//  Created by Md Altaf Hoshain Firoj on 5/6/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIWithMVVMApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
